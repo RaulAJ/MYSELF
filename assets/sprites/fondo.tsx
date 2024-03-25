e61c4e37b1221e33e651cc3cf5a29adebdc877b2
MarioAlc <alc2mario@gmail.com>
2024-03-25 22:29:24 +0100

    mapa zona 1

---
+++ assets/sprites/fondo.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="fondo" tilewidth="616" tileheight="304" tilecount="4" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="544" height="236" source="Environment/PNG/clouds.png"/>
+ </tile>
+ <tile id="1">
+  <image width="616" height="110" source="Environment/PNG/far-grounds.png"/>
+ </tile>
+ <tile id="2">
+  <image width="112" height="96" source="Environment/PNG/sea.png"/>
+ </tile>
+ <tile id="3">
+  <image width="112" height="304" source="Environment/PNG/sky.png"/>
+ </tile>
+</tileset>
---
+++ assets/sprites/tileset_castillo.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tileset_castillo" tilewidth="32" tileheight="32" tilecount="182" columns="26">
+ <image source="Old-dark-Castle-tileset-Files/PNG/old-dark-castle-interior-tileset.png" width="832" height="240"/>
+</tileset>
